a64f5fa949db0de65e3187339aa50e3a774123af
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-01 14:47:20 +0700

    feat: add layout header,content, footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu.tsx";
 
 export default function Footer() {
     return (
-        <div className="h-[200px] p-12 lg:px-28 bg-lightGreen flex flex-col lg:flex-row lg:items-center gap-4 justify-center lg:justify-between">
+        <div className="h-[200px] p-8 md:p-12 bg-lightGreen flex flex-col lg:flex-row lg:items-center gap-4 justify-center lg:justify-between">
             <Logo textColor={"text-white"}/>
             <Menu showLogin={false} textColor={"text-white"} underlineColor={"lg:before:bg-white"} />
         </div>
