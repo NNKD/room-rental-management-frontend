1ea1d1fed59705711948a7f619404d924b26bfe3
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-01 14:41:48 +0700

    fix: fix header and seperate logo, site name to component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 // import {Link} from "react-router-dom";
 
-import Footer from "../components/Footer.tsx";
+import Header from "../components/Header.tsx";
 
 export default function Home() {
     return (
         <div>
             {/*<p className="text-amber-500 text-2xl">Home Page</p>*/}
             {/*<Link className="underline" to="/apartments">Go to Apartment List</Link>*/}
-            <Footer/>
+            <Header/>
         </div>
     )
 }
