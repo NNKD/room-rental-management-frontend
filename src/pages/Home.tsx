4c759c33c2c7349de916f3bc297156a480cc1f31
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-01 14:08:48 +0700

    fix: return home.tsx same main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
-// import {Link} from "react-router-dom";
-import Header from "../components/Header.tsx";
+import {Link} from "react-router-dom";
 
 export default function Home() {
     return (
         <div>
-            {/*<p className="text-amber-500 text-2xl">Home Page</p>*/}
-            {/*<Link className="underline" to="/apartments">Go to Apartment List</Link>*/}
-            <Header />
+            <p className="text-amber-500 text-2xl">Home Page</p>
+            <Link className="underline" to="/apartments">Go to Apartment List</Link>
         </div>
     )
 }
