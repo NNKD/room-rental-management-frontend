58ca0177340737584381ae27477e51e1a926ca8a
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-01 14:41:48 +0700

    fix: fix header and seperate logo, site name to component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Menu from "./Menu.tsx";
 import {IoMenu} from "react-icons/io5";
 import {IoIosClose} from "react-icons/io";
 import {useState} from "react";
+import Logo from "./Logo.tsx";
 
 export default function Header() {
     const [showSideBar, setShowSideBar] = useState(false)
@@ -10,10 +11,7 @@ export default function Header() {
 
     return (
         <div className="flex items-center justify-between select-none">
-            <div className="flex items-center gap-4">
-                <PiBuildingApartmentBold className="w-8 h-8 md:w-10 md:h-10"/>
-                <h1 className="text-xl md:text-2xl font-semibold">Name site</h1>
-            </div>
+            <Logo textColor={"text-black"}/>
 
             {/*mobile show sidebar, desktop hide*/}
             <div className="p-2 cursor-pointer" onClick={() => setShowSideBar(!showSideBar)}>
@@ -22,11 +20,11 @@ export default function Header() {
 
             {showSideBar ? (
                 <div className="fixed top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.3)] lg:hidden" onClick={() => setShowSideBar(!showSideBar)}>
-                    <div className="w-2/5 h-full ml-auto bg-white animate-slide-right-to-left-500" onClick={(e) => e.stopPropagation()}>
+                    <div className="w-2/5 h-full ml-auto text-center bg-white animate-slide-right-to-left-500" onClick={(e) => e.stopPropagation()}>
                         <div className="p-2 ml-auto w-fit cursor-pointer" onClick={() => setShowSideBar(!showSideBar)}>
                             <IoIosClose className="text-4xl" />
                         </div>
-                        <Menu showLogin={true}/>
+                        <Menu showLogin={true} textColor={"text-black"} underlineColor={"lg:before:bg-black"}/>
                     </div>
                 </div>
             ) : ("")}
@@ -34,7 +32,7 @@ export default function Header() {
 
             {/*desktop show menu*/}
             <div className="hidden lg:block">
-                <Menu showLogin={true}/>
+                <Menu showLogin={true} textColor={"text-black"} underlineColor={"lg:before:bg-black"}/>
             </div>
         </div>
     )
