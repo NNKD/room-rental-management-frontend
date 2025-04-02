4e400f007171749027e0a8d2ca2977784affd774
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-02 15:40:55 +0700

    feat(apartment-list): add search

---
+++ src/components/Search.tsx
@@ -0,0 +1,59 @@
+import {searchBathroomOption, searchBedroomOption, searchTypeOption} from "../data.ts";
+import {CiSearch} from "react-icons/ci";
+
+export default function Search({setName, setType, setBedroom, setBathroom}:
+                               {setName: (value: string) => void,
+                               setType: (value: string) => void,
+                               setBedroom: (value: string) => void,
+                               setBathroom: (value: string) => void,}) {
+
+    return (
+        <div className="flex items-center gap-4 flex-wrap">
+            <div className="flex flex-col lg:flex-row gap-4 flex-grow">
+                <input placeholder="Search by name" className="border border-darkGray rounded py-4 lg:py-2 px-4 outline-none lg:flex-grow" onChange={(e) => setName(e.target.value)}/>
+
+                <div className="border outline-none border-darkGray rounded py-4 lg:py-2 px-4">
+                    <select className="outline-none pr-2 text-darkGray w-full select-none cursor-pointer" defaultValue="" onChange={(e) => setType(e.target.value)}>
+                        <option value="" disabled>Loại căn hộ</option>
+
+                        {searchTypeOption.map((option, index) => (
+                            <option key={index} value={option.value} className="text-black">{option.name}</option>
+                        ))}
+
+                    </select>
+                </div>
+            </div>
+
+            <div className="flex flex-col lg:flex-row gap-4 flex-grow lg:w-min">
+                <div className="border outline-none border-darkGray rounded py-4 lg:py-2 px-4">
+                    <select className="outline-none pr-2 text-darkGray w-full select-none cursor-pointer" defaultValue="" onChange={(e) => setBedroom(e.target.value)}>
+                        <option value="" disabled>Số phòng ngủ</option>
+
+                        {searchBedroomOption.map((option, index) => (
+                            <option key={index} value={option.value} className="text-black">{option.name}</option>
+                        ))}
+
+                    </select>
+                </div>
+
+                <div className="border outline-none border-darkGray rounded py-4 lg:py-2 px-4">
+                    <select className="outline-none pr-2 text-darkGray w-full select-none cursor-pointer" defaultValue="" onChange={(e) => setBathroom(e.target.value)}>
+                        <option value="" disabled>Số phòng tắm</option>
+
+                        {searchBathroomOption.map((option, index) => (
+                            <option key={index} value={option.value} className="text-black">{option.name}</option>
+                        ))}
+
+                    </select>
+                </div>
+            </div>
+
+            <div className="
+            bg-lightGreen text-white w-full lg:w-fit flex items-center justify-center py-4 lg:py-2 px-4 gap-2 rounded
+            select-none cursor-pointer hover:bg-lightGreenHover transition-all duration-300 ease-in-out">
+                <CiSearch className="text-xl" />
+                <span className="font-semibold text-base">Search</span>
+            </div>
+        </div>
+    )
+}
--- src/pages/ApartmentList.tsx
+++ src/pages/ApartmentList.tsx
@@ -1,10 +1,29 @@
-import {Link} from "react-router-dom";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+import {useEffect, useState} from "react";
+import Search from "../components/Search.tsx";
+
 
 export default function ApartmentList() {
+
+    const [name, setName] = useState("")
+    const [type, setType] = useState("")
+    const [bedroom, setBedroom] = useState("")
+    const [bathroom, setBathroom] = useState("")
+
+    useEffect(() => {
+        console.log(name, type, bedroom, bathroom)
+    }, [name, type, bedroom, bathroom])
+
     return (
-        <div>
-            <p className="text-amber-800 text-2xl">Apartment List</p>
-            <Link className="underline" to="/apartment/can-ho-101">Go to Apartment Detail 101</Link>
+        <div className="flex flex-col min-h-screen">
+            <Header/>
+
+            <div className="flex-grow p-8 md:p-12">
+                <Search setName={setName} setType={setType} setBedroom={setBedroom} setBathroom={setBathroom}/>
+            </div>
+
+            <Footer/>
         </div>
     )
 }
