11f31bb7b02bf045f71a4dd9b452c397ffa15bf6
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-02 18:53:50 +0700

    feat(apartment-list): add component apartment item

--- src/pages/ApartmentList.tsx
+++ src/pages/ApartmentList.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
 import {useEffect, useState} from "react";
 import Search from "../components/Search.tsx";
+import ApartmentItem from "../components/ApartmentItem.tsx";
 
 
 export default function ApartmentList() {
@@ -21,7 +22,17 @@ export default function ApartmentList() {
 
             <div className="flex-grow p-8 md:p-12">
                 <Search setName={setName} setType={setType} setBedroom={setBedroom} setBathroom={setBathroom} showName={true}/>
+
+                <div className="grid grid-rows-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-y-12 mt-20 justify-between">
+                    <ApartmentItem />
+                    <ApartmentItem />
+                    <ApartmentItem />
+                    <ApartmentItem />
+                    <ApartmentItem />
+                </div>
             </div>
+
+
 
             <Footer/>
         </div>
