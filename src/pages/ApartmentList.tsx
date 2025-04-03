8ac8df5f50707c7c0d686a79a235e8b5152046d7
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-03 14:20:47 +0700

    feat(apartment-list): add sort by price and show total item

--- src/pages/ApartmentList.tsx
+++ src/pages/ApartmentList.tsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 import Search from "../components/Search.tsx";
 import ApartmentItem from "../components/ApartmentItem.tsx";
 import {apartmentList} from "../data.ts";
-import {ApartmentListType} from "../type.ts";
+import {ApartmentListItem} from "../type.ts";
 
 export default function ApartmentList() {
 
@@ -12,10 +12,25 @@ export default function ApartmentList() {
     const [type, setType] = useState("")
     const [bedroom, setBedroom] = useState("")
     const [bathroom, setBathroom] = useState("")
+    const [sort, setSort] = useState("")
+    const [apartments, setApartments] = useState(apartmentList)
 
     useEffect(() => {
         console.log(name, type, bedroom, bathroom)
     }, [name, type, bedroom, bathroom])
+
+    useEffect(() => {
+        if (sort != "") {
+            console.log(sort)
+            handleSortByPrice()
+        }
+    }, [sort]);
+
+    const handleSortByPrice = () => {
+        const copy = [...apartments]
+        const sorted = copy.sort((a : ApartmentListItem, b : ApartmentListItem) => sort == "asc" ? (a.price - b.price) : (b.price - a.price))
+        setApartments(sorted)
+    }
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -24,9 +39,20 @@ export default function ApartmentList() {
             <div className="flex-grow p-8 md:p-12">
                 <Search setName={setName} setType={setType} setBedroom={setBedroom} setBathroom={setBathroom} showName={true}/>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-y-12 mt-20 justify-between">
+                <div className="flex items-center justify-between w-full border-t border-lightGray mt-8 pt-8">
+                    <p className="text-xl">6 kết quả</p>
+                    <div className="border outline-none border-darkGray rounded py-4 lg:py-2 px-4">
+                        <select className="outline-none pr-2 text-darkGray select-none cursor-pointer" defaultValue="" onChange={(e) => setSort(e.target.value)}>
+                            <option value="" disabled>Sắp xếp</option>
+                            <option value="asc">Giá tăng dần</option>
+                            <option value="desc">Giá giảm dần</option>
+                        </select>
+                    </div>
+                </div>
 
-                    {apartmentList.map((apartment: ApartmentListType) => (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-y-12 mt-6 justify-between">
+
+                    {apartments.map((apartment: ApartmentListItem) => (
                         <ApartmentItem key={apartment.id} apartment={apartment} />
                     ))}
 
