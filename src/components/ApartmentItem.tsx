885affa5fb8e4d35b4f0fe64f08001c2aff48853
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-04-04 09:46:33 +0700

    fix(apartment-list): fix apartment item to correct with image aspect ratio and layout

--- src/components/ApartmentItem.tsx
+++ src/components/ApartmentItem.tsx
@@ -5,10 +5,12 @@ import {Link} from "react-router-dom";
 export default function ApartmentItem({apartment}: {apartment: ApartmentListItem}) {
     return (
         <Link to={`/apartment/${apartment.slug}`}
-              className="w-fit mx-auto rounded shadow-[0_0_4px_2px_#ddd]
-                        hover:shadow-[0_0_6px_4px_#bbb] transition-all duration-300 ease-in-out">
-            <img src={apartment.image} className="rounded-t" alt={`${apartment.name}-image`}/>
-            <div className="p-4">
+              className="w-full mx-auto rounded shadow-[0_0_4px_2px_#ddd]
+                        hover:shadow-[0_0_6px_4px_#bbb] transition-all duration-300 ease-in-out flex flex-col h-full">
+            <div className="overflow-hidden rounded-t aspect-[4/3] flex-shrink-0">
+                <img src={apartment.image} className="w-full h-full object-cover rounded-t hover:scale-125 transition-all duration-300 ease-in-out" alt={`${apartment.name}-image`}/>
+            </div>
+            <div className="p-4 flex-grow ">
                 <h2 className="font-bold text-xl leading-[1]">{apartment.brief}</h2>
                 <p className="text-base text-lightGreen font-bold my-2">{formatCurrency(apartment.price)}</p>
                 <p className="text-base">{apartment.name}</p>
