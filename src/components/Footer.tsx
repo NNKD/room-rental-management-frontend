5122749418facd8bd3050fae7f5febd4b695824e
Vo Nam Ngan Ha <vonamnganha0611@gmail.com>
2025-04-26 09:52:24 +0700

    chang minscream

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu.tsx";
 
 export default function Footer() {
     return (
-        <div className="h-[240px] p-8 md:p-12 bg-lightGreen flex flex-col lg:flex-row lg:items-center gap-4 justify-center lg:justify-between">
+        <div className="h-[100px] p-8 md:p-12 bg-lightGreen flex flex-col lg:flex-row lg:items-center gap-4 justify-center lg:justify-between">
             <Logo textColor={"text-white"}/>
             <Menu showLogin={false} textColor={"text-white"} underlineColor={"lg:before:bg-white"} />
         </div>
