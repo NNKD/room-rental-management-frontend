4015a4d303db0e87f312f3273330b0aa4aeb6959
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-05-21 14:29:16 +0700

    refactor(home): refactor code show apartment, search, feat add slider

--- src/components/ApartmentItem.tsx
+++ src/components/ApartmentItem.tsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 export default function ApartmentItem({apartment}: {apartment: ApartmentListItem}) {
     return (
         <Link to={`/apartment/${apartment.slug}`}
-              className="w-full mx-auto rounded shadow-[0_0_4px_2px_#ddd]
+              className="w-full mx-auto rounded shadow-[0_0_4px_2px_#ddd] select-none
                         hover:shadow-[0_0_6px_4px_#bbb] transition-all duration-300 ease-in-out flex flex-col h-full">
             <div className="overflow-hidden rounded-t aspect-[4/3] flex-shrink-0">
                 <img loading="lazy" src={apartment.image} className="w-full h-full object-cover rounded-t hover:scale-125 transition-all duration-300 ease-in-out" alt={`${apartment.name}-image`}/>
