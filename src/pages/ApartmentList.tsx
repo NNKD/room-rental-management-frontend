aab73e53bca8736b08facb7442205d5baa6705be
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-05-23 09:46:55 +0700

    feat(apartment-list-connect-api): connect api show list apartment, pagination

--- src/pages/ApartmentList.tsx
+++ src/pages/ApartmentList.tsx
@@ -3,14 +3,17 @@ import Footer from "../components/Footer.tsx";
 import {useEffect, useState} from "react";
 import Search from "../components/Search.tsx";
 import ApartmentItem from "../components/ApartmentItem.tsx";
-import {apartmentPages} from "../data.ts";
-import {ApartmentListItem} from "../type.ts";
 import {
     MdKeyboardArrowLeft,
     MdKeyboardArrowRight,
     MdKeyboardDoubleArrowLeft,
     MdKeyboardDoubleArrowRight
 } from "react-icons/md";
+import axios from "axios";
+import {envVar} from "../utils/EnvironmentVariables.ts";
+import {ApartmentListItem} from "../types/Apartment.ts";
+import SkeletonApartmentItem from "../components/skeleton-loading/SkeletonApartmentItem.tsx";
+import {PageType} from "../types/PageList.ts";
 
 export default function ApartmentList() {
 
@@ -19,28 +22,53 @@ export default function ApartmentList() {
     const [bedroom, setBedroom] = useState("")
     const [bathroom, setBathroom] = useState("")
     const [sort, setSort] = useState("")
-    const [apartments, setApartments] = useState(apartmentPages.content)
-    const [pages, setPages] = useState<number[]>([])
-    const [currentPage, setCurrentPage] = useState(apartmentPages.number)
+    const [apartments, setApartments] = useState<ApartmentListItem[]>([])
+    const [pages, setPages] = useState<PageType<ApartmentListItem> | null>(null)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [pageArray, setPageArray] = useState<number[]>([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         console.log(name, type, bedroom, bathroom)
     }, [name, type, bedroom, bathroom])
 
     useEffect(() => {
+        handleGetApartments()
+    }, [currentPage])
+
+    const handleGetApartments = async () => {
+        setLoading(true)
+        try {
+            const response = await axios.get(`${envVar.API_URL}/apartments`, {
+                params: {
+                    page: currentPage,
+                }
+            });
+
+            if (response.status == 200 && response.data.status == "success" && response.data.statusCode == 200) {
+                console.log(response.data)
+                setPages(response.data.data)
+            }
+
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        setCurrentPage(pages?.pageNumber || 1)
+        setApartments(pages?.list || [])
+        if (pages) {
+            handleShowPage()
+            setLoading(false)
+        }
+    }, [pages]);
+
+    useEffect(() => {
         if (sort != "") {
             handleSortByPrice()
         }
     }, [sort]);
-
-    useEffect(() => {
-        setCurrentPage(apartmentPages.number)
-        handleShowPage()
-    }, []);
-
-    useEffect(() => {
-        handleShowPage()
-    }, [currentPage]);
 
     const handleSortByPrice = () => {
         const copy = [...apartments]
@@ -51,9 +79,8 @@ export default function ApartmentList() {
     // Calculation page number to show 3 number
     const handleShowPage = () => {
         const startPage = Math.max(1, currentPage - 1)
-        const endPage = Math.min(currentPage + 1, apartmentPages.totalPages)
-
-        setPages(Array.from({length: endPage - startPage + 1}, (_, index) => startPage + index))
+        const endPage = Math.min(currentPage + 1, (pages?.totalPages || currentPage))
+        setPageArray(Array.from({length: endPage - startPage + 1}, (_, index) => startPage + index))
     }
 
     /*
@@ -77,7 +104,7 @@ export default function ApartmentList() {
                 setCurrentPage(currentPage + 1)
                 break
             case 2:
-                setCurrentPage(apartmentPages.totalPages)
+                setCurrentPage(pages?.totalPages || currentPage)
                 break
             default:
                 setCurrentPage(pageNumber ?? 1)
@@ -105,9 +132,13 @@ export default function ApartmentList() {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 gap-y-12 mt-6 justify-between">
 
-                    {apartments.map((apartment: ApartmentListItem) => (
-                        <ApartmentItem key={apartment.id} apartment={apartment} />
-                    ))}
+                    {loading ? Array(6).fill(0).map((_, index) => (
+                        <SkeletonApartmentItem key={index}/>
+                    )) : (
+                        apartments.map((apartment: ApartmentListItem) => (
+                            <ApartmentItem key={apartment.id} apartment={apartment} />
+                        ))
+                    )}
 
                 </div>
 
@@ -128,7 +159,7 @@ export default function ApartmentList() {
                                     ${currentPage == 1 ? "text-lightGray border-lightGray pointer-events-none" : "text-lightGreen border-lightGreen cursor-pointer hover:bg-lightGreen hover:text-white duration-300 ease-in-out"}`}
                     onClick={() => handleChangePage(-1, 0)}/>
 
-                    {pages.map((pageNumber: number) => (
+                    {pageArray.map((pageNumber: number) => (
                         <span key={pageNumber}
                               className={`w-[30px] h-[30px] md:w-[36px] md:h-[36px] text-xl md:text-2xl border-2 text-center
                                     ${currentPage == pageNumber ? "text-white border-lightGreen bg-lightGreen pointer-events-none" : "text-lightGreen border-lightGreen cursor-pointer hover:bg-lightGreen hover:text-white duration-300 ease-in-out"}`}
@@ -139,12 +170,12 @@ export default function ApartmentList() {
 
                     <MdKeyboardArrowRight
                         className={`w-[30px] h-[30px] md:w-[36px] md:h-[36px] text-3xl md:text-4xl border-2
-                                    ${currentPage == apartmentPages.totalPages ? "text-lightGray border-lightGray pointer-events-none" : "text-lightGreen border-lightGreen cursor-pointer hover:bg-lightGreen hover:text-white duration-300 ease-in-out"}`}
+                                    ${currentPage == pages?.totalPages ? "text-lightGray border-lightGray pointer-events-none" : "text-lightGreen border-lightGreen cursor-pointer hover:bg-lightGreen hover:text-white duration-300 ease-in-out"}`}
                     onClick={() => handleChangePage(1, 0)}/>
 
                     <MdKeyboardDoubleArrowRight
                         className={`w-[30px] h-[30px] md:w-[36px] md:h-[36px] text-3xl md:text-4xl border-2
-                                    ${currentPage == apartmentPages.totalPages ? "text-lightGray border-lightGray pointer-events-none" : "text-lightGreen border-lightGreen cursor-pointer hover:bg-lightGreen hover:text-white duration-300 ease-in-out"}`}
+                                    ${currentPage == pages?.totalPages ? "text-lightGray border-lightGray pointer-events-none" : "text-lightGreen border-lightGreen cursor-pointer hover:bg-lightGreen hover:text-white duration-300 ease-in-out"}`}
                         onClick={() => handleChangePage(2, 0)}/>
 
                 </div>
--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,5 @@
-export interface ApartmentListItem {
-    id: number;
-    name: string;
-    slug: string;
-    brief: string;
-    price: number;
-    image: string;
-}
+
+
 
 /*
     T is data type Ex: ApartmentManagementType, ApartmentTypeManagementType, ...
