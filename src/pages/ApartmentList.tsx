5b09a174b2bf3e16dc4016e083a9323bf5d3cfbf
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-05-30 11:25:43 +0700

    fix(apartment detail): fix send form

--- src/pages/ApartmentList.tsx
+++ src/pages/ApartmentList.tsx
@@ -226,7 +226,7 @@ export default function ApartmentList() {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 gap-y-12 mt-6 justify-between">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 gap-6 gap-y-12 mt-6 justify-between">
                     {loading ? (
                         Array(6).fill(0).map((_, index) => (
                             <SkeletonApartmentItem key={index} />
