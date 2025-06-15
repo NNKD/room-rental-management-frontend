52c54d9e175353aa780885a0724bcca047980e33
Vo Nam Ngan Ha <vonamnganha0611@gmail.com>
2025-06-15 10:55:00 +0700

    them chuc nang da ngon ngu

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import NoticeUI from "./components/NoticeUI.tsx";
 import { AuthProvider } from "./contexts/AuthContext";
 import { LoadingProvider } from "./contexts/LoadingContext.tsx";
 import LoadingApi from "./components/LoadingApi.tsx";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
 export default function App() {
     return (
-        <AuthProvider>
-            <NoticeProvider>
-                <LoadingProvider>
-                    <Outlet />
-                    <NoticeUI />
-                    <LoadingApi />
-                </LoadingProvider>
-            </NoticeProvider>
-        </AuthProvider>
+        <I18nextProvider i18n={i18n}>
+            <AuthProvider>
+                <NoticeProvider>
+                    <LoadingProvider>
+                        <Outlet />
+                        <NoticeUI />
+                        <LoadingApi />
+                    </LoadingProvider>
+                </NoticeProvider>
+            </AuthProvider>
+        </I18nextProvider>
     );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,68 +1,51 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-/*
-    showLogin: use to hide or show Đăng nhập. Value true | false
-    textColor: use to set text color. Value: text-white, text-black, text-[#ccc],...
-    underlineColor: use to set underline color when hover. Value: lg:before:bg-white, lg:before:bg-black, lg:before:bg-[#ccc]
-*/
+interface MenuProps {
+    showLogin: boolean;
+    textColor: string;
+    underlineColor: string;
+}
 
-export default function Menu({showLogin, textColor, underlineColor} : {showLogin: boolean, textColor: string, underlineColor: string}) {
+export default function Menu({ showLogin, textColor, underlineColor }: MenuProps) {
+    const { t } = useTranslation();
+
     return (
         <div className={`flex flex-col lg:flex-row md:items-center md:gap-8 lg:gap-12`}>
-            <Link to="/"
-                  className={` relative
-                     lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2
-                     lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full
-                     ${underlineColor}
-                     lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out
-                     text-base ${textColor}
-                     py-2`}>
-                Trang chủ
+            <Link
+                to="/"
+                className={`relative lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2 lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full ${underlineColor} lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out text-base ${textColor} py-2`}
+            >
+                {t("home")}
             </Link>
-            <Link to="/apartments"
-                  className={` relative
-                     lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2
-                     lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full
-                     ${underlineColor}
-                     lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out
-                     text-base ${textColor}
-                     py-2`}>
-                Căn hộ
+            <Link
+                to="/apartments"
+                className={`relative lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2 lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full ${underlineColor} lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out text-base ${textColor} py-2`}
+            >
+                {t("apartments")}
             </Link>
-            <Link to="/amenities"
-                  className={` relative
-                     lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2
-                     lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full
-                     ${underlineColor}
-                     lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out
-                     text-base ${textColor}
-                     py-2`}>
-                Tiện ích
+            <Link
+                to="/amenities"
+                className={`relative lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2 lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full ${underlineColor} lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out text-base ${textColor} py-2`}
+            >
+                {t("amenities")}
             </Link>
-            <Link to="/contact"
-                  className={` relative
-                     lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2
-                     lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full
-                     ${underlineColor}
-                     lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out
-                     text-base ${textColor}
-                     py-2`}>
-                Liên hệ
+            <Link
+                to="/contact"
+                className={`relative lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2 lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full ${underlineColor} lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out text-base ${textColor} py-2`}
+            >
+                {t("contact")}
             </Link>
-
             {showLogin ? (
-                <Link to="/login"
-                      className={` relative
-                        lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2
-                        lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full
-                        ${underlineColor}
-                        lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out
-                        text-base ${textColor}
-                        py-2`}>
-                    Đăng nhập
+                <Link
+                    to="/login"
+                    className={`relative lg:before:absolute lg:before:bottom-0 lg:before:left-1/2 lg:before:-translate-x-1/2 lg:before:w-0 lg:before:h-0.5 lg:hover:before:w-full ${underlineColor} lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out text-base ${textColor} py-2`}
+                >
+                    {t("login")}
                 </Link>
-            ) : ("")}
-
+            ) : (
+                ""
+            )}
         </div>
-    )
+    );
 }
